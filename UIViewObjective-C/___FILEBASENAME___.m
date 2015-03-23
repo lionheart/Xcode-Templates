1e889d9e3399ad6d68761c96d90ce7179067e1ce
Dan Loewenherz <dloewenherz@gmail.com>
2015-03-23 16:12:55 -0500

    initial commit

---
+++ UIViewObjective-C/___FILEBASENAME___.m
@@ -0,0 +1,21 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#import "___FILEBASENAME___.h"
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+
+    }
+    return self;
+}
+
+@end
